0be75b524a6367d0e436e3a70150d081f17efe9f
DebTheDevV <debthedevv@gmail.com>
2025-11-23 21:34:01 +0530

    src code added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+package src;
+
+ import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        AppGUI window = new AppGUI();
+        window.pack();
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
